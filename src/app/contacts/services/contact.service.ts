0504b4650ec04b074d7cbde86b18ddbd975990c2
JasLieb <jason.liebault@outlook.com>
2020-03-18 22:00:28 +0100

    Fix PR comment

--- src/app/contacts/services/contact.service.ts
+++ src/app/contacts/services/contact.service.ts
@@ -1,69 +1,71 @@
-import { Injectable, EventEmitter } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs/';
-import { Contact, ContactState } from '../../models/contact';
+import { Contact, ContactState, State } from '../../models/contact';
 import produce from 'immer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
-  contactState: ContactState;
   contactStateBehavior: BehaviorSubject<ContactState>;
 
-  addUpdater = produce((state: ContactState, contactName: string) => {
-    state.contacts.push(
-      new Contact(contactName, false)
-    );
-
-    state.contacts = state.contacts.sort(
-      (a, b) => a.name.localeCompare(b.name)
-    );
-  });
-
-  favouriteContactUpdater = produce((state: ContactState, contactIndex: number) => {
-    state.contacts[contactIndex].isFavourite = ! state.contacts[contactIndex].isFavourite;
-  });
-
-  onlyFavouriteUpdater = produce((state: ContactState, onlyFavourite: boolean) => {
-    state.onlyFavourites = onlyFavourite;
-  });
-
   constructor() {
-    this.contactState = new ContactState([
-        new Contact('tata', false),
-        new Contact('tete', false),
-        new Contact('titi', false),
-        new Contact('toto', true),
-        new Contact('tutu', false)
-      ],
-      false
+    this.contactStateBehavior = new BehaviorSubject<ContactState>(
+      new ContactState(
+        [
+          new Contact('tata', false),
+          new Contact('tete', false),
+          new Contact('titi', false),
+          new Contact('toto', true),
+          new Contact('tutu', false)
+        ],
+        false
+      )
     );
-
-    this.contactStateBehavior = new BehaviorSubject<ContactState>(this.contactState);
   }
 
-  setState(state: ContactState) {
-    this.contactState = state;
-    this.contactStateBehavior.next(this.contactState);
+  private setState(updater: (state: ContactState) => ContactState) {
+    this.contactStateBehavior
+      .next(
+        updater( this.contactStateBehavior.getValue() )
+      );
   }
 
   addNewContact(contactName: string) {
     if (contactName.length > 0) {
       this.setState(
-        this.addUpdater(this.contactState, contactName)
+        produce(
+          (state: ContactState) => {
+            state.contacts.push(
+              new Contact(contactName, false)
+            );
+
+            state.contacts = state.contacts.sort(
+              (a, b) => a.name.localeCompare(b.name)
+            );
+          }
+        )
       );
     }
   }
 
   toggleFavourite(contactIndex: number) {
     this.setState(
-      this.favouriteContactUpdater(this.contactState, contactIndex)
+      produce(
+        (state: ContactState) => {
+          state.contacts[contactIndex].isFavourite = ! state.contacts[contactIndex].isFavourite;
+        }
+      )
     );
   }
 
   showFavourites(onlyFavourite: boolean) {
-    this.contactStateBehavior.next(
-      this.onlyFavouriteUpdater(this.contactState, onlyFavourite)
+    this.setState(
+      produce(
+        (state: ContactState) => {
+          state.onlyFavourites = onlyFavourite;
+        }
+      )
     );
   }
 }
